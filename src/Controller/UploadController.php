157ba129abe9aab5fab53a47a6f53c0f98bf37c4
Stanislas Raczynski <stan.racz7@gmail.com>
2019-10-17 21:49:28 +0200

    ajout conditions et render de uploadcontroller

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -10,8 +10,8 @@ use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use App\Entity\Competition;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-
 
 
 class UploadController extends AbstractController
@@ -19,19 +19,26 @@ class UploadController extends AbstractController
     /**
      * @Route("/upload", name="uploadTab")
      */
-    public function upload()
+    public function upload(Request $request)
     {
         $compet = new Competition();
+        $compet->
         $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $compet);
         $formBuilder
             ->add('heureDepart', TimeType::class)
             ->add('cadence', TimeType::class)
-            ->add('fichier', FileType::class)
-            ->add('save', SubmitType::class)
-            ;
+            ->add('fichier', FileType::)
+            ->add('save', SubmitType::class);
         $form = $formBuilder->getForm();
         return $this->render('upload/upload.html.twig', array(
             'form' => $form->createView(),
         ));
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            if ($form->isValid()) {
+
+            }
+        }
     }
 }
