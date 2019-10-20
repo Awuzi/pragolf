12fe43df9ca72e8f47f7a61e718fd2a14e7e2769
Stan <stan.racz7@gmail.com>
2019-10-20 17:20:02 +0200

    ajout de commentaire à UploadController

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -21,15 +21,24 @@ class UploadController extends AbstractController
      */
     public function upload(Request $request)
     {
+        //instanciation d'un objet competition
         $competition = new Competition();
+        //creation du formulaire et liasion avec l'entité competition
         $form = $this->createForm(UploadFormType::class, $competition);
         $form->handleRequest($request);
+        //si le formulaire est soumis et valide alors:
         if ($form->isSubmitted() && $form->isValid()) {
+            //récuperation du fichier qui a été uploadé
             $file = $competition->getFichier();
+            //stockage du futur nom du fichier que notre application connait
             $filename = "Fichier"."."."xlsx";
+            //déplacement du fichier dans l'upload directory dont le chemin
+            //est specifié dans services.yaml
+            //chemin: '%kernel.project_dir%/public/assets/doc'
             $file->move($this->getParameter('upload_directory'), $filename);
+            //renommage du fichier
             $competition->setFichier($filename);
-
+            //redirection vers la vue "view"
             return $this->redirectToRoute("view");
         }
 
