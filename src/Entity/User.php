9867558ddc7f1ac23ef908845eabfd088b6f6347
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2020-02-14 21:54:29 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -45,7 +45,7 @@ class User implements UserInterface
      */
     public function getUsername(): string
     {
-        return (string)$this->username;
+        return (string) $this->username;
     }
 
     public function setUsername(string $username): self
@@ -81,7 +81,7 @@ class User implements UserInterface
      */
     public function getPassword(): string
     {
-        return (string)$this->password;
+        return (string) $this->password;
     }
 
     public function setPassword(string $password): self
